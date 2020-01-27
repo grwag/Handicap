7a48843f699a521a2a6697fcd263e873b68de331
grwag <gregor.wagner@live.com>
2020-01-27 18:59:11 +0100

    .

--- Handicap.Api/ClientApp/src/app/auth.config.ts
+++ Handicap.Api/ClientApp/src/app/auth.config.ts
@@ -19,5 +19,5 @@ export const authConfig: AuthConfig = {
   // set the scope for the permissions the client should request
   // The first three are defined by OIDC. The 4th is a usecase-specific one
   scope: 'read read_write openid profile email offline_access',
-  postLogoutRedirectUri: 'http://localhost:4200',
+  postLogoutRedirectUri: 'https://localhost:5001',
 };
